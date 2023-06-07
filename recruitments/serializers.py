f2ebbf26d737e0493831792b20819b60409934de
Lee DoYoon <ehdbs1276@naver.com>
2023-06-07 19:20:47 +0900

    [추가]동료모집 수정, 삭제 기능 추가
    동료 모집 게시글을 수정, 삭제할 수 있습니다.
    
    [추가]recruitments/views.py
    - RecruitmentDetailView에 put과 delete부분을 추가하였습니다.
    
    [추가]recruitments/serializers.py
    - RecruitmentEditSerializer를 추가하였습니다.

--- recruitments/serializers.py
+++ recruitments/serializers.py
@@ -10,7 +10,7 @@ class RecruitmentSerializer(serializers.ModelSerializer):
         fields = ("id", "user", "title", "content", "image", "created_at", "updated_at",) # 미구현 추가 내용 "category", "rate", "comment_count", "like_count",
 
     def get_user(self, obj):
-        return {"id":obj.user.id, "nickname":obj.user.email}
+        return {"id":obj.user.id, "nickname":obj.user.email} # nickname이 email인 부분 추후 수정 예정 / 지금은 제가 임시로 쓰는 user가 그렇게 되어있다보니....
         
 
 class RecruitmentDetailSerializer(serializers.ModelSerializer):
@@ -23,4 +23,16 @@ class RecruitmentDetailSerializer(serializers.ModelSerializer):
     def get_user(self, obj):
         return {"id":obj.user.id, "nickname":obj.user.email}
         # return {"id":obj.user.id, "nickname":obj.user.email, "age":obj.user.age, "gender":obj.user.gender}
+        # 임시로 사용중인 user모델에 age, gender가 없어서 잠시 주석처리 해놓았습니다.
         
+class RecruitmentEditSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Recruitments
+        fields = ("id", "user", "title", "image", "place", "departure", "arrival", "content",) # 미구현 추가 내용 "category",
+
+        
+    def get_user(self, obj):
+        return {"id":obj.user.id, "nickname":obj.user.email}
+        # return {"id":obj.user.id, "nickname":obj.user.email, "age":obj.user.age, "gender":obj.user.gender}
--- recruitments/views.py
+++ recruitments/views.py
@@ -8,6 +8,7 @@ from recruitments.models import Recruitments, Applicant
 from recruitments.serializers import (
     RecruitmentSerializer,
     RecruitmentDetailSerializer,
+    RecruitmentEditSerializer,
 )
 
 
@@ -33,10 +34,24 @@ class RecruitmentDetailView(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request, recruitment_id):
-        pass
+        recruitments = get_object_or_404(Recruitments, id=recruitment_id)
+        serializer = RecruitmentEditSerializer(recruitments, data=request.data)
+        if recruitments.user == request.user:
+            if serializer.is_valid():
+                serializer.save(user=request.user)
+                return Response(({"message":"동료 모집 수정 완료"},serializer.data), status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({"message":"권한이 없습니다"}, status=status.HTTP_403_FORBIDDEN)
     
     def delete(self, request, recruitment_id):
-        pass
+        recruitments = get_object_or_404(Recruitments, id=recruitment_id)
+        if recruitments.user == request.user:
+            recruitments.delete()
+            return Response({"message":"동료 모집 삭제 완료"}, status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response({"message":"권한이 없습니다"}, status=status.HTTP_403_FORBIDDEN)
 
 
 class RecruitmentJoinView(APIView):
