4731e3c9f713ffe1d792dbc90418e7390be41511
wooseok <william741@naver.com>
2023-06-08 10:57:13 +0900

    [추가] 여행경로 모델 추가

--- routes/models.py
+++ routes/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Routes(models.Model):
+    
+    user = models.ForeignKey(User, verbose_name="작성자", on_delete=models.CASCADE, related_name="user")
+    title = models.CharField("제목", max_length=50)
+    content = models.TextField("내용")
+    created_at = models.DateTimeField("작성시각", auto_now_add=True)
+    updated_at = models.DateTimeField("수정시각", auto_now=True)
+    image = models.ImageField("이미지", upload_to="article/%Y/%m/", blank=True)
+    cost = models.IntegerField("경비")
+    duration = models.CharField("기간")
+    
+    
+    def __str__(self):
+        return self.title
+
+
+class Destinations(models.Model):
+    route = models.ForeignKey(Routes, verbose_name="경로", on_delete=models.CASCADE, related_name="route")
+    area_code = models.IntegerField("지역코드")
+    sigungu_code = models.IntegerField("시군구코드")
+    
+
+class Places(models.Model):
+    route = models.ForeignKey(Routes, verbose_name="경로", on_delete=models.CASCADE, related_name="route")
+    content_id = models.IntegerField("컨텐트아이디")
+    content_type_id = models.IntegerField("컨텐트타입아이디")
