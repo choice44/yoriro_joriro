c7a7a14893759f7e211c36fe23351467409ca18d
jeanallen928 <123160000+jeanallen928@users.noreply.github.com>
2023-06-08 12:24:09 +0900

    [추가]paths, 리뷰 CRUD 뼈대, 좋아요
    reviews/urls.py에 path들을 추가하였습니다.
    path들에 쓰인 리뷰 CRUD View들의 뼈대를 만들었습니다.
    리뷰 좋아요를 추가했습니다.

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,72 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from reviews.models import Review
 
-# Create your views here.
+
+# reviews/
+class ReviewView(APIView):
+    # 로그인한 사람은 리뷰 작성 가능. 아니면 리뷰 전체 목록 조회만 가능.
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    """
+    리뷰 전체 목록 조회
+    """
+
+    def get(self, request):
+        reviews = Review.objects.all().order_by("-created_at")
+        pass
+
+    """
+    리뷰 작성
+    """
+
+    def post(self, request):
+        pass
+
+
+# reviews/<int:review_id>/
+class ReviewDetailView(APIView):
+    # 로그인한 사람은 리뷰 수정/삭제 가능. 아니면 리뷰 상세보기만 가능.
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    """
+    리뷰 상세보기
+    """
+
+    def get(self, request, review_id):
+        review = get_object_or_404(Review, id=review_id)
+        pass
+
+    """
+    리뷰 수정하기
+    """
+
+    def put(self, request, review_id):
+        review = get_object_or_404(Review, id=review_id)
+        pass
+
+    """
+    리뷰 삭제하기
+    """
+
+    def delete(self, request, review_id):
+        review = get_object_or_404(Review, id=review_id)
+        pass
+
+
+# reviews/<int:review_id>/like/
+class ReviewLikeView(APIView):
+    # 로그인한 사람만 좋아요 가능
+    permission_classes = [permissions.IsAuthenticated]
+    """
+    리뷰 좋아요
+    """
+
+    def post(self, request, review_id):
+        review = get_object_or_404(Review, id = review_id)
+        if request.user in review.likes.all():
+            review.likes.remove(request.user)
+            return Response({"message":"좋아요를 취소했습니다."}, status=status.HTTP_200_OK)
+        else:
+            review.likes.add(request.user)
+            return Response({"message":"좋아요를 눌렀습니다."}, status=status.HTTP_200_OK)
