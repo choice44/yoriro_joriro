e84d2da337b838668069ec89e121ea5c987af7d2
Lee DoYoon <ehdbs1276@naver.com>
2023-06-12 12:28:36 +0900

    [수정]participator_count 이름 변경
    [수정]paritipator_count를 participant_max로 바꾸고, '모집정원'으로 이름을
    변경하였습니다.
    
    [수정]recruitments/modes.py
    - Recruitments모델에 있는 participator_count를 participant_max로
      변경하였습니다.
    
    [수정]recruitments/serializers.py
    - 유저 디테일 페이지에서 현재 모집된 인원을 파악하기 위해
      participant_now를 추가하였습니다.(확인용이기 때문에 이후 삭제되거나
      수정될 예정입니다.)

--- recruitments/serializers.py
+++ recruitments/serializers.py
@@ -16,20 +16,23 @@ class RecruitmentSerializer(serializers.ModelSerializer):
 
 class RecruitmentDetailSerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField()    
-    participant = serializers.SerializerMethodField()    
+    participant = serializers.SerializerMethodField()
+    participant_now = serializers.SerializerMethodField()
 
     class Meta:
         model = Recruitments
         fields = "__all__"
 
     def get_user(self, obj):
-        return {"id":obj.user.id, "nickname":obj.user.nickname}
-        # return {"id":obj.user.id, "nickname":obj.user.email, "age":obj.user.age, "gender":obj.user.gender}
-        # user모델에 age, gender가 없어서 잠시 주석처리 해놓았습니다.
+        return {"id":obj.user.id, "nickname":obj.user.email, "age":obj.user.age, "gender":obj.user.gender}
         
     def get_participant(self, obj):
         participant_data = obj.participant.values("id", 'nickname')
         return participant_data
+    
+    def get_participant_now(self, obj):
+        participant_now_data = obj.participant.count()
+        return participant_now_data
 
 
 class RecruitmentEditSerializer(serializers.ModelSerializer):
@@ -37,17 +40,16 @@ class RecruitmentEditSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recruitments
-        fields = ("id", "user", "title", "image", "place", "departure", "arrival", "content",) # 미구현 추가 내용 "category",
+        fields = ("id", "user", "title", "image", "place", "departure", "arrival", "content",) # 미구현 추가  내용 "category",
         
     def get_user(self, obj):
-        return {"id":obj.user.id, "nickname":obj.user.nickname}
-        # return {"id":obj.user.id, "nickname":obj.user.email, "age":obj.user.age, "gender":obj.user.gender}
+        return {"id":obj.user.id, "nickname":obj.user.email, "age":obj.user.age, "gender":obj.user.gender}
 
 
 class RecruitmentJoinSerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField()
 
-    class Meta:
+    class Meta: 
         model = Applicant
         fields = ("id", "user", "appeal", "acceptence",)
 
