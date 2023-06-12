a52fcf855de78076ce35a8f200186cb28b8469bb
Choi Seungwon <cso6549@naver.com>
2023-06-12 14:35:06 +0900

    [추가] 조리로 ai 추가

--- joriro/models.py
+++ joriro/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Joriro(models.Model):
+    user = models.ForeignKey(User, verbose_name="작성자",
+                             on_delete=models.SET_NULL, related_name="joriros", null=True)
+    image = models.ImageField("이미지", upload_to="joriro/%Y/%m/")
+    model = models.PositiveIntegerField("모델")
+    place = models.PositiveIntegerField("여행지")
+    created_at = models.DateTimeField("작성일", auto_now_add=True)
+    updated_at = models.DateTimeField("수정일", auto_now=True)
--- joriro/views.py
+++ joriro/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.generics import get_object_or_404
 
-# Create your views here.
+from .serializers import JoriroSerializer, Joriro
+from .ai import predict, fit_background, blend
+from .apps import JoriroConfig
+
+from PIL import Image
+import numpy as np
+import cv2
+import os
+
+
+class JoriroView(APIView):
+    def post(self, request):
+        serializer = JoriroSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        img = Image.open(serializer.data["image"][1:])
+        background = np.array(Image.open(
+            JoriroConfig.backgrounds[serializer.data["place"]]))
+        model = JoriroConfig.models[serializer.data["model"]]
+        weights = JoriroConfig.weights[serializer.data["model"]]
+
+        mask = predict(model, weights, img)
+
+        img = np.array(img)
+
+        background = fit_background(img, background)
+
+        result = blend(img, mask, background)
+
+        save_path = "media/joriro/result/"
+
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+
+        cv2.imwrite(save_path + str(serializer.data["id"]) + ".jpg", result)
+
+        return Response({"image": f"/media/joriro/result/{serializer.data['id']}.jpg"})
