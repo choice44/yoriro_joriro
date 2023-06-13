3828203fb2a92042d80111222b4d0799875607c8
wooseok <william741@naver.com>
2023-06-13 19:51:51 +0900

    [추가] 시리얼라이저, views에 주석 추가

--- routes/serializers.py
+++ routes/serializers.py
@@ -15,13 +15,13 @@ class SigunguSerializer(serializers.ModelSerializer):
         model = Sigungu
         fields = "__all__"
 
-# 스팟
+# 관광지, 맛집
 class SpotSerializer(serializers.ModelSerializer):
     class Meta:
         model = Spot
         fields = "__all__"      
 
-# 루트 지역        
+# 여행경로 지역       
 class RouteAreaSerializer(serializers.ModelSerializer):
     class Meta:
         model = RouteArea
@@ -34,12 +34,15 @@ class RouteSerializer(serializers.ModelSerializer):
     rate = serializers.SerializerMethodField()
     areas = RouteAreaSerializer(many=True)
 
+    # 유저 정보 가져오기
     def get_user(self, obj):
         return {'id': obj.user.pk, 'nickname': obj.user.nickname}
 
+    # 댓글 갯수 카운트
     def get_comment_count(self, obj):
         return obj.comments.count()
 
+    # 해당 게시글의 평점을 모두 가져온 후 평균값 내기
     def get_rate(self, obj):
         rate_avg = obj.rates.aggregate(average=Avg('rate'))['average']
         return rate_avg
@@ -48,10 +51,12 @@ class RouteSerializer(serializers.ModelSerializer):
         model = Route
         fields = "__all__"
 
+
 # 댓글 조회
 class CommentSerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField()
 
+    # 유저 정보 가져오기
     def get_user(self, obj):
         return {'id': obj.user.pk, 'nickname': obj.user.nickname}
 
@@ -68,11 +73,14 @@ class RouteCreateSerializer(serializers.ModelSerializer):
         fields = ('title', 'title', 'content', 'image', 'cost', 'duration', 'routespot', 'areas')
     
     def create(self, validated_data):
+        # 루트지역과 루트스팟 데이터 가져오기
+        # 얘네 둘은 다대다 관계로 되어 있어서 특별 취급이 필요함
         route_area_data = validated_data.pop('areas')
         routespot_data = validated_data.pop('routespot')
 
+        # 모델 생성
+        # RouteArea모델은 route가 인자로 필요함
         route = Route.objects.create(**validated_data)
-        
         routearea = RouteArea.objects.create(route=route, **route_area_data)
         
         # 리스트 형태로 전달하면 리스트 자체를 하나의 인자로 처리
@@ -81,21 +89,25 @@ class RouteCreateSerializer(serializers.ModelSerializer):
         return route
      
     def update(self, instance, validated_data):
+        # 루트지역과 루트스팟 데이터 가져오기
         route_area_data = validated_data.pop('areas')
         routespot_data = validated_data.pop('routespot')
         
+        # 새로운 정보를 기존 정보에 덮어 씌우기
         instance.title = validated_data.get('title', instance.title)
         instance.content = validated_data.get('content', instance.content)
         instance.image = validated_data.get('image', instance.image)
         instance.duration = validated_data.get('duration', instance.duration)
         instance.cost = validated_data.get('cost', instance.cost)
         
+        # set()메서드는 다대다관계에서 관련된 객체들을 대체함
         instance.routespot.set(routespot_data)
-        
+
+        # areas에 있는 기존 데이터를 삭제하고 새로 기입된 정보로 새로운 모델 생성
         instance.areas.all().delete()
-        
         routearea = RouteArea.objects.create(route=instance, **route_area_data)
         
+        # 변경정보 저장
         instance.save()
         return instance
     
--- routes/views.py
+++ routes/views.py
@@ -3,7 +3,7 @@ from rest_framework.generics import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from routes.models import Route, Comment, RouteRate, Spot
+from routes.models import Route, Comment, RouteRate
 from routes.serializers import (
     RouteSerializer,
     RouteCreateSerializer,
@@ -19,7 +19,7 @@ class RouteView(APIView):
 
     # 여행루트 조회
     def get(self, request):
-        routes = Route.objects.all().order_by("-created_at")
+        routes = Route.objects.all().order_by("-created_at")    # 최신순 정렬
         serializer = RouteSerializer(routes, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -27,14 +27,16 @@ class RouteView(APIView):
     def post(self, request):
         serializer = RouteCreateSerializer(data=request.data)
         
+        # 유저가 로그인 상태가 아닐 때
         if not request.user.is_authenticated:
             return Response({"message": "권한이 없습니다."}, status=status.HTTP_403_FORBIDDEN)
 
+        # 유저가 기입한 정보가 유효하지 않을 때
         if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
+        # 위의 if문 2개를 통과하면 저장
         serializer.save(user=request.user)
-
         return Response(({"message": "여행 루트 작성 완료!"},serializer.data), status=status.HTTP_201_CREATED)
 
 
@@ -44,11 +46,11 @@ class SpotCreateView(APIView):
     def post(self, request):
         serializer = SpotSerializer(data=request.data)
         
+        # 유저가 기입한 정보가 유효하지 않을 때
         if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
         serializer.save()
-
         return Response(({"message": "스팟 등록 완료!"},serializer.data), status=status.HTTP_201_CREATED)
 
 
@@ -67,14 +69,16 @@ class RouteDetailView(APIView):
         route = get_object_or_404(Route, id=route_id)
         serializer = RouteCreateSerializer(route, data=request.data)
 
+        # 유저정보가 일치하지 않을 때
         if route.user != request.user:
             return Response({"message": "권한이 없습니다."}, status=status.HTTP_403_FORBIDDEN)
 
+        # 기입된 정보가 유효하지 않을 때
         if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
+        # if문 통과 후 저장
         serializer.save(user=request.user)
-
         return Response(({"message": "여행 루트 수정 완료!"},serializer.data), status=status.HTTP_200_OK)
 
             
@@ -82,9 +86,11 @@ class RouteDetailView(APIView):
     def delete(self, request, route_id):
         route = get_object_or_404(Route, id=route_id)
         
+        # 유저정보가 일치하지 않을 때
         if route.user != request.user:
             return Response({"message": "권한이 없습니다."}, status=status.HTTP_403_FORBIDDEN)
 
+        # 여행루트 삭제
         route.delete()
         return Response({"message": "여행 루트 삭제 완료!"}, status=status.HTTP_204_NO_CONTENT)
             
@@ -97,7 +103,7 @@ class CommentView(APIView):
     # 댓글 전체 조회
     def get(self, request, route_id):
         routes = get_object_or_404(Route, id=route_id)
-        comments = routes.comments.all().order_by("-created_at")
+        comments = routes.comments.all().order_by("-created_at")    # 최신순 정렬
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -105,14 +111,16 @@ class CommentView(APIView):
     def post(self, request, route_id):
         serializer = CommentCreateSerializer(data=request.data)
         
+        # 유저가 로그인 상태가 아닐 때
         if not request.user.is_authenticated:
             return Response({"message": "권한이 없습니다."}, status=status.HTTP_403_FORBIDDEN)
 
+        # 기입된 정보가 유효하지 않을 때
         if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
+        # if문 통과 후 저장
         serializer.save(user=request.user, route_id=route_id)
-
         return Response(({"message": "댓글 작성 완료!"},serializer.data), status=status.HTTP_201_CREATED)
         
 
@@ -124,22 +132,32 @@ class CommentDetailView(APIView):
     def put(self, request, comment_id):
         comment = get_object_or_404(Comment, id=comment_id)
         serializer = CommentCreateSerializer(comment, data=request.data)
-        if comment.user == request.user:
-            if serializer.is_valid():
-                serializer.save(user=request.user)
-                return Response(({"message": "댓글 수정 완료!"}, serializer.data), status=status.HTTP_200_OK)
-        else:
+        
+        # 유저정보가 일치하지 않을 때
+        if comment.user != request.user:
             return Response({"message": "권한이 없습니다."}, status=status.HTTP_403_FORBIDDEN)
+        
+        # 기입된 정보가 유효하지 않을 때
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        # if문 통과 후 저장
+        serializer.save(user=request.user)
+        return Response(({"message": "댓글 수정 완료!"},serializer.data), status=status.HTTP_200_OK)
+        
 
     # 댓글 삭제
     def delete(self, request, comment_id):
         comment = get_object_or_404(Comment, id=comment_id)
-        if comment.user == request.user:
-            comment.delete()
-            return Response({"message": "댓글 삭제 완료!"}, status=status.HTTP_204_NO_CONTENT)
-        else:
+        
+        # 유저정보가 일치하지 않을 때
+        if comment.user != request.user:
             return Response({"message": "권한이 없습니다."}, status=status.HTTP_403_FORBIDDEN)
 
+        # 여행루트 삭제
+        comment.delete()
+        return Response({"message": "댓글 삭제 완료!"}, status=status.HTTP_204_NO_CONTENT)
+    
 
 class RateView(APIView):
     # 로그인한 사람만 평점을 줄 수 있음
@@ -156,12 +174,15 @@ class RateView(APIView):
 
         try:
             rate = int(rate)
+            
+            # rate가 0보다 작거나 100보다 크다면 400에러
             if rate < 0 or rate > 100:
                 return Response({"message": "평점은 0부터 100까지의 정수로 입력해주세요."}, status=status.HTTP_400_BAD_REQUEST)
-        except ValueError:
+            
+        except ValueError:  # ValueError가 나왔을경우 400에러
             return Response({"message": "평점은 0부터 100까지의 정수로 입력해주세요."}, status=status.HTTP_400_BAD_REQUEST)
 
-        # route_rate는 새로 생성했거나 사져온 RouteRate 객체
+        # route_rate는 새로 생성했거나 가져온 RouteRate 객체
         # created는 새로 생성된 객체인지 나타내는 bool값
         route_rate, created = RouteRate.objects.get_or_create(route=route, user=request.user)
         route_rate.rate = rate
