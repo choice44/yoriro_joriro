8b31c2c740ae384fed7c79eadca48d83137a2516
Choi Seungwon <cso6549@naver.com>
2023-06-15 11:42:50 +0900

    [추가] 스팟 앱 추가

---
+++ spots/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from spots.models import Area, Sigungu, Spot
+
+
+admin.site.register(Area)
+admin.site.register(Sigungu)
+admin.site.register(Spot)
--- yoriro_joriro/settings.py
+++ yoriro_joriro/settings.py
@@ -50,6 +50,8 @@ INSTALLED_APPS = [
     "recruitments",
     "routes",
     "joriro",
+    "spots",
+    "django_filters"
 ]
 
 MIDDLEWARE = [
@@ -161,7 +163,9 @@ AUTH_USER_MODEL = "users.User"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 6,
 }
 
 
