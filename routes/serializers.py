32bfbc4e1e50fd1a22156bd432e47d7ea5191fd4
wooseok <william741@naver.com>
2023-06-20 12:54:10 +0900

    [수정] 여행루트 게시글 저장을 위한 시리얼라이저 수정

--- routes/serializers.py
+++ routes/serializers.py
@@ -2,8 +2,9 @@ from rest_framework import serializers
 from django.db.models import Avg
 
 from .models import Route, Comment, RouteArea
-from spots.serializers import SpotSerializer
 
+from spots.models import Area
+from spots.serializers import SpotSerializer
 
 # 여행경로 지역
 class RouteAreaSerializer(serializers.ModelSerializer):
@@ -52,19 +53,37 @@ class CommentSerializer(serializers.ModelSerializer):
 
 # 여행경로 작성, 수정
 class RouteCreateSerializer(serializers.ModelSerializer):
-    areas = RouteAreaSerializer()
+    areas = serializers.JSONField()
 
     class Meta:
         model = Route
         fields = ('title', 'title', 'content', 'image',
                   'cost', 'duration', 'spots', 'areas')
+        
+    # 파이썬 객체를 클라이언트에 보낼 수 있는 형식(json)으로 바꿔주는 메서드
+    # instance = Route입니다.
+    def to_representation(self, instance):
+        # instance의 모든 필드를 기본형식으로 변환 -> 결과를 data라는 딕셔너리로 저장
+        data = super().to_representation(instance)
+        
+        # data에 areas의 값을 바꿔줍니다.
+        # RouteAreaSerializer의 instance는 RouteArea인데 areas로 엮인 모든 객체를 직렬화
+        # data['areas']의 값은 RouteAreaSerializer로 직렬화된 RouteArea 객체의 데이터
+        data['areas'] = RouteAreaSerializer(instance.areas.all(), many=True).data
+        
+        return data
 
     def create(self, validated_data):
         # 루트지역과 루트스팟 데이터 가져오기
         # 얘네 둘은 다대다 관계로 되어 있어서 특별 취급이 필요함
+        print(validated_data)
         route_area_data = validated_data.pop('areas')
         spots_data = validated_data.pop('spots')
 
+        # areas데이터의 area id를 이용해 Area객체를 데이터베이스에서 가져옴
+        area = Area.objects.get(pk=route_area_data['area'])
+        route_area_data['area'] = area
+        
         # 모델 생성
         # RouteArea모델은 route가 인자로 필요함
         route = Route.objects.create(**validated_data)
