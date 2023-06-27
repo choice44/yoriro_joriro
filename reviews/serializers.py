64dfa2b4576168937c017c6d7ba1514f27f2b204
jeanallen928 <123160000+jeanallen928@users.noreply.github.com>
2023-06-27 12:27:15 +0900

    [수정]리뷰 상세보기에 작성자 프로필 이미지 추가

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -46,6 +46,7 @@ class ReviewListSerializer(serializers.ModelSerializer):
 # 리뷰 상세 조회        
 class ReviewDetailSerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField()
+    user_image = serializers.SerializerMethodField()
     spot = serializers.SerializerMethodField()
     created_at = serializers.SerializerMethodField()
     updated_at = serializers.SerializerMethodField()
@@ -54,6 +55,13 @@ class ReviewDetailSerializer(serializers.ModelSerializer):
     
     def get_user(self, obj):
         return {"id": obj.user.id, "nickname": obj.user.nickname}
+    
+    
+    def get_user_image(self, obj):
+        if obj.user.image:
+            return obj.user.image.url
+        else:
+            return None
     
     
     def get_spot(self, obj):
