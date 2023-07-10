3d3ea4ca6d36e25b1a2b9c58313c1b16e53ad075
Choi Seungwon <cso6549@naver.com>
2023-07-10 17:03:49 +0900

    [수정] 불필요한 url 패턴 제거

--- yoriro_joriro/urls.py
+++ yoriro_joriro/urls.py
@@ -13,6 +13,3 @@ urlpatterns = [
     path("joriro/", include("joriro.urls")),
     path("spots/", include('spots.urls')),
 ]
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
